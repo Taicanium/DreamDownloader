1ef25056283794363994401c2fe6dcbecde78e1a
Taicanium <SirLevalioDaBurnshire@gmail.com>
2023-09-04 23:44:42 -0500

    Simplified the Regex replacement process.

--- DreamDownloader.ps1
+++ DreamDownloader.ps1
@@ -1,44 +1,13 @@
 <# There are certain names on the PokeAPI website that include
 identifiers (-normal, -male, etc) that Bulbagarden doesn't use.
-So, we create a List of changes to make to those names, so that
-Bulbagarden CAN read them. This List is thirty-or-so names long
-but that's still a lot easier than listing all 1,000 Pokemon in
-the file directly. #>
+We can modify these names so that Bulbagarden CAN recognize
+them, but three names require specific changes beyond just
+cutting everything off past a '-'. #>
 
 $replacements = @(
-
-	@{Name1="nidoran-f";Name2="nidoran"},
 	@{Name1="farfetchd";Name2="farfetch'd"},
 	@{Name1="mr-mime";Name2="mr. mime"},
-	@{Name1="deoxys-normal";Name2="deoxys"},
-	@{Name1="wormadam-plant";Name2="wormadam"},
-	@{Name1="giratina-altered";Name2="giratina"},
-	@{Name1="shaymin-land";Name2="shaymin"},
-	@{Name1="basculin-red-striped";Name2="basculin"},
-	@{Name1="darmanitan-standard";Name2="darmanitan"},
-	@{Name1="tornadus-incarnate";Name2="tornadus"},
-	@{Name1="thundurus-incarnate";Name2="thundurus"},
-	@{Name1="landorus-incarnate";Name2="landorus"},
-	@{Name1="keldeo-ordinary";Name2="keldeo"},
-	@{Name1="meloetta-aria";Name2="meloetta"},
-	@{Name1="meowstic-male";Name2="meowstic"},
-	@{Name1="aegislash-shield";Name2="aegislash"},
-	@{Name1="pumpkaboo-average";Name2="pumpkaboo"},
-	@{Name1="gourgeist-average";Name2="gourgeist"},
-	@{Name1="zygarde-50";Name2="zygarde"},
-	@{Name1="oricorio-baile";Name2="oricorio"},
-	@{Name1="lycanroc-midday";Name2="lycanroc"},
-	@{Name1="wishiwashi-solo";Name2="wishiwashi"},
-	@{Name1="minior-red-meteor";Name2="minior"},
-	@{Name1="toxtricity-amped";Name2="toxtricity"},
-	@{Name1="mr-rime";Name2="mr. rime"},
-	@{Name1="eiscue-ice";Name2="eiscue"},
-	@{Name1="indeedee-male";Name2="indeedee"},
-	@{Name1="morpeko-full-belly";Name2="morpeko"},
-	@{Name1="urshifu-single-strike";Name2="urshifu"},
-	@{Name1="basculegion-male";Name2="basculegion"},
-	@{Name1="enamorus-incarnate";Name2="enamorus"}
-
+	@{Name1="mr-rime";Name2="mr. rime"}
 )
 
 $pokemonCount = 1007 <# This number doesn't include Paradox Pokemon, because they have a skill issue. #>
@@ -47,9 +16,7 @@ $pokemonSearchURI = "pokeapi.co/api/v2/pokemon/" <# We start with this base web 
 
 $allURI = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0" <# We'll call this web address if and only if the user wants to download all Pokemon at once. This will get us all the names, rather than going one-by-one via index. #>
 
-$searchClass = "fullMedia" <# In Bulbagarden's backend HTML, this is the identifier for the actual image file that it shows on a search results page. #>
-
-$mainURI = "https://archives.bulbagarden.net/w/index.php" <# We'll call the Bulbagarden API to initiate a raw search... #>
+$mainURI = "https://archives.bulbagarden.net/w/index.php" <# We'll call the Bulbagarden frontend API to initiate a raw search... #>
 
 $subURI = "?title=Special:Search&limit=500&offset=0&profile=images&search=" <# ...and we'll use these parameters while narrowing things down by Pokemon name and index. #>
 
@@ -84,23 +51,25 @@ function SearchForFilesBySpecies
 
 	catch [System.Net.WebException] { <# If PokeAPI returns an error, that means it didn't recognize the species name. #>
 
-		Write-Host 'Unrecognized species. Script will attempt to search regardless.' <# However, let's account for that just in case the user is searching for a newly added 'mon that PokeAPI hasn't added yet. #>
+		Write-Host 'Unrecognized species. Script will attempt to search regardless.' <# However, let's account for that just in case the user is searching for a newly created 'mon that PokeAPI hasn't added yet. #>
 
 		$requestedIndex = Read-Host -Prompt 'Species index' <# Ask the user to specify the index manually. #>
 	}
 
-	$replaced = $false <# Start by assuming that the species name DOESN'T fall into the replacement table at the start of this file. #>
 	foreach ($replacement in $replacements) {
-		if ($Species.Equals($replacement.Name1)) {
+		if ($Species.Equals($replacement.Name1)) { <# If the species name falls into the replacement List at the start of the file... #>
 
-			$Species = $replacement.Name2 <# If it does, however, then actually do the replacement. Exchange "mr-mime" for "mr. mime", etc. #>
+			Write-Host 'Exchanging' $Species 'for' $replacement.Name2'.' <# ...advise the user of the change... #>
+			$Species = $replacement.Name2 <# ...and actually do the replacement. Exchange "mr-mime" for "mr. mime", etc. #>
 			$replaced = $true
 		}
 	}
 
+	$Species = $Species -replace "-.*",""
+
 	$req = Invoke-Webrequest -URI $mainURI$subURI$requestedIndex$Species <# Now that we have our species name and our Pokedex number, make the search request to Bulbagarden. #>
 
-	$images = $req.Images.src <# Give ourselves a short name for the web addresses to each file result. PowerShell makes this easy: In a lower-level language like C++, we'd have to iterate through each item in the List and take out the 'src' elements one at a time. But by requesting the 'src' element on the List itself, as if the List was a single object, PowerShell allows us to grab the 'src' element from all items in the List on a single line. #>
+	$images = $req.Images.src <# Give ourselves a short name for the web addresses to each file result. PowerShell makes this easy: In a lower-level language like C++, we'd have to iterate through each item in the List and take out the 'src' elements one at a time. #>
 
 	Write-Host 'Discovered' ($images.Count - 1) 'files matching' $Species'. Saving...' <# Let the user know how many search results we found. We subtract 1 from this count because it includes a MediaWiki logo present on the search results page. #>
 
@@ -113,6 +82,13 @@ function SearchForFilesBySpecies
 			$finalName = $finalUri -replace ".*/" <# Grab the file's actual name by removing the rest of the web address. #>
 
 			$fileReq = Invoke-Webrequest $finalUri -OutFile $finalName <# Finally, now that we have the file name and address, call a web request directly to the file, and save it locally. #>
+			
+			<# This is also why we used the frontend API (/w/index.php) rather than the developer API (/w/api.php):
+			The developer search function links us to the file namespace pages on Bulbagarden Archives, but not the
+			files directly. That means we'd have to make three web requests per file - one to the API, one to the
+			file page, and one to the raw file. By manipulating URLs on the frontend search page, we can save
+			ourselves one of those requests. Since a full search easily runs into the tens of thousands of files,
+			that translates to a huge amount of web traffic and thereby time saved. #>
 		}
 	}
 }
@@ -171,7 +147,7 @@ if ($requestedSpecies.Equals('all')) { <# Ask the user if they want to download 
 			}
 		}
 
-		catch [System.Net.WebException] { } <# If an error occurs, do not halt the script. Discard the species we tried to download, and otherwise continue the search as normal. #>
+		catch [System.Net.WebException] { } <# If an error occurs, do not exit the script. Discard the species we tried to download, and otherwise continue the search as normal. #>
 	}
 } else {
 
